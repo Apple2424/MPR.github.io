920730a2b73901a5dc9904713ba0fc5062fea7e6
Apple24 <42840053+Apple2424@users.noreply.github.com>
2024-01-21 01:50:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function showAlert() {
-    alert('Button Clicked!');
-}
-
-function fadeInComplete() {
-    // This function is called when the fade-in animation is complete.
-    // You can add additional actions here, such as fading out and navigating to a new page.
-    fadeOutAndNavigate();
-}
-
 function fadeOutAndNavigate() {
     // Get the element to fade out (in this case, the div with class "fade-in").
     const fadeElement = document.querySelector('.fade-in');
@@ -15,9 +5,9 @@ function fadeOutAndNavigate() {
     // Add a class to initiate the fade-out animation.
     fadeElement.classList.add('fade-out');
 
-    // After a delay, navigate to a new page.
+    // After a delay, navigate to the new page.
     setTimeout(() => {
         // Change the URL to the desired new page.
-        window.location.href = 'new_page.html';
+        window.location.href = 'play.html';
     }, 1000); // Adjust the delay time (in milliseconds) based on your preferences.
 }
